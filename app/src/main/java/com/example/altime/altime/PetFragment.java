bd8a9eceb9592c5e9289c6c255008f0ba1eff16d
NathanM00 <nathanmanuelsolano@hotmail.com>
2018-11-23 22:50:36 -0500

    Mascota full intento #300
    --Nathan M
    
    Signed-off-by: NathanM00 <NathanM00>
    Signed-off-by: ValHappy <ValHappy>

--- app/src/main/java/com/example/altime/altime/PetFragment.java
+++ app/src/main/java/com/example/altime/altime/PetFragment.java
@@ -3,17 +3,91 @@ package com.example.altime.altime;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class PetFragment extends Fragment {
 
+    View vista;
+    FloatingActionButton btn_petMenu;
+    ProgressBar pgb_Felicidad;
+    ProgressBar pgb_Energia;
+    int felicidad =100;
+    int energia=80;
+    boolean puesto= false;
+    ImageView franckie;
+    Bundle bundle = new Bundle();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(getArguments() !=null) {
+            felicidad = getArguments().getInt("felicidad");
+            energia = getArguments().getInt("energia");
+            puesto = getArguments().getBoolean("puesto");
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_pet, container,false);
+        vista = inflater.inflate(R.layout.fragment_pet, container, false);
+        btn_petMenu = vista.findViewById(R.id.btn_petMenu);
+        pgb_Felicidad = vista.findViewById(R.id.pgb_felicidad);
+       pgb_Energia = vista.findViewById(R.id.pgb_energia);
+        franckie = vista.findViewById(R.id.iv_franckiePet);
+
+        if(!puesto) {
+            franckie.setImageResource(R.drawable.mascota_petedit);
+            puesto = false;
+            bundle.putBoolean("puesto", puesto);
+
+        }else{
+            franckie.setImageResource(R.drawable.franc_cap);
+            puesto = true;
+            bundle.putBoolean("puesto", puesto);
+        }
+
+        btn_petMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new PetEditFragment();
+                fragment.setArguments(bundle);
+                FragmentTransaction transaction  = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_contenedor, fragment);
+                transaction.commit();
+
+            }
+        });
+
+        pgb_Felicidad.setProgress(felicidad);
+        pgb_Energia.setProgress(energia);
+        barraProgreso();
+
+        return vista;
     }
+
+    private  void barraProgreso(){
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (felicidad >=0 && energia <=1500){
+                    felicidad--;
+                    energia++;
+                    android.os.SystemClock.sleep(1500);
+                    pgb_Felicidad.setProgress(felicidad);
+                    pgb_Energia.setProgress(energia);
+                }
+            }
+        }).start();
+    }
+
 }
