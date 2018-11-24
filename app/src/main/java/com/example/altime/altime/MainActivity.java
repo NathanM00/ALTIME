5a6c6efa42038c91c57fcc67b6795d2d42cfa0f8
NathanM00 <nathanmanuelsolano@hotmail.com>
2018-11-24 00:58:54 -0500

    ya ni se
    --Nathan M
    
    Signed-off-by: NathanM00 <NathanM00>
    Signed-off-by: ValHappy <ValHappy>

--- app/src/main/java/com/example/altime/altime/MainActivity.java
+++ app/src/main/java/com/example/altime/altime/MainActivity.java
@@ -65,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
                         break;
 
                     case R.id.nav_cal:
-
                         fragment = new CalendarFragment();
 
                         estudiando = false;
